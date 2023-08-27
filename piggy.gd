f0c46b697c1da893d53bd81ad29d7316586067a1
Wallace Oliveira <wallacebatistaoliveira@gmail.com>
2023-08-27 19:35:13 -0300

    piggy

---
+++ piggy.gd
@@ -0,0 +1,33 @@
+extends Area2D
+
+@export var speed = 70
+
+@onready var animation_player = $AnimationPlayer
+@onready var sprite_2d = $Sprite2D
+
+func _process(delta):
+	var input_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	
+	if input_vector == Vector2.ZERO:
+		animation_player.play("idle")
+	else:
+		animation_player.play("run")
+		
+		if input_vector.x != 0:
+			# rotate sprite based on input vector
+			sprite_2d.scale.x = sign(input_vector.x)
+		
+		#if input_vector.x > 0:
+		#	sprite_2d.scale.x = 1
+		#else:
+		#	sprite_2d.scale.x = -1
+	
+	
+	position += input_vector * speed * delta
+
+
+
+func _on_area_entered(area):
+	# destroy the apples
+	area.queue_free()
+	scale *= 1.1
